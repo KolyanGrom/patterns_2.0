1da2dfdb165fe30377e9a199178925a9cb452cd9
KolyanGrom <kolyangrom@yandex.ru>
2024-04-24 00:13:25 +0700

    change project code

--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -1,20 +1,46 @@
 package ru.netology.testmode.test;
 
 import com.codeborne.selenide.Condition;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.netology.testmode.data.DataGenerator;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.restassured.RestAssured.given;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
 import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
 import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
 
 class AuthTest {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @BeforeAll
+    static void setUpAll() {
+
+        given()
+                .spec(requestSpec)
+                .body(new DataGenerator.RegistrationDto("vasya", "password", "active"))
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
 
     @BeforeEach
     void setup() {
